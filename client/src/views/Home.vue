4aad913acc7b40bfedb6fe502613bc203d31f20c
Nenad <nbursa@gmail.com>
2024-06-18 23:30:03 +0200

    Cleanup client

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-2xl mx-auto p-4 pb-24 font-light">
-    <h1 class="text-4xl font-bold mb-6 py-6 text-center">Model Builder</h1>
+    <h1 class="text-4xl font-bold mb-6 py-6 text-center">Model Trainer</h1>
     <form @submit.prevent="trainModel" class="space-y-6">
       <div class="flex flex-col items-center justify-center text-sm">
         <div class="text-gray-400 mb-4">
