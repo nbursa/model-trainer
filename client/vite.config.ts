78a4d9084339424fd8c62bd97a294bdc42a274c6
Nenad <nbursa@gmail.com>
2024-06-19 08:42:23 +0200

    Cleanup client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
